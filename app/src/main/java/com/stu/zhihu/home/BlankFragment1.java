885838244cd1e5d1ee6f5b5a0fe47724c63f90fa
hsh40 <huangshihang1>
2019-06-27 15:47:06 +0800

    第3版本

--- app/src/main/java/com/stu/zhihu/home/BlankFragment1.java
+++ app/src/main/java/com/stu/zhihu/home/BlankFragment1.java
@@ -41,7 +41,7 @@ public class BlankFragment1 extends Fragment {
 
 
         initView(view);
-
+        sw.setRefreshing(true);
         sw.setColorSchemeColors(Color.parseColor("#018dff"));
         sw.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -56,52 +56,59 @@ public class BlankFragment1 extends Fragment {
         });
         final List<Data1> list = new ArrayList<>();
         for (int i = 90; i < 999; i++) {
-            list.add(new Data1("挂科是世界上最恐怖的事情!", "据统计,没10秒就有一个人因挂科死亡据统计,没10秒就有一个人因挂科死亡", i+"赞同 .  "+206+i+"评论"));
+            list.add(new Data1("挂科是世界上最恐怖的事情!", "据统计,没10秒就有一个人因挂科死亡据统计,没10秒就有一个人因挂科死亡", i + "赞同 .  " + 206 + i + "评论"));
         }
-         list.add(new Data1("挂科是世界上最恐怖的事情!", "据统计,没10秒就有一个人因挂科死亡据统计,没10秒就有一个人因挂科死亡", ""));
+        list.add(new Data1("挂科是世界上最恐怖的事情!", "据统计,没10秒就有一个人因挂科死亡据统计,没10秒就有一个人因挂科死亡", ""));
+        list.add(new Data1("挂科是世界上最恐怖的事情!", "据统计,没10秒就有一个人因挂科死亡据统计,没10秒就有一个人因挂科死亡", ""));
         list.add(new Data1("挂科是世界上最恐怖的事情!", "据统计,没10秒就有一个人因挂科死亡据统计,没10秒就有一个人因挂科死亡", ""));
         list.add(new Data1("挂科是世界上最恐怖的事情!", "据统计,没10秒就有一个人因挂科死亡据统计,没10秒就有一个人因挂科死亡", ""));
         list.add(new Data1("挂科是世界上最恐怖的事情!", "据统计,没10秒就有一个人因挂科死亡据统计,没10秒就有一个人因挂科死亡", ""));
 
         rec.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rec.setAdapter(new RecyclerView.Adapter() {
-           class ViewHolder  extends RecyclerView.ViewHolder{
-                public View rootView;
-                public TextView t1;
-                public TextView t2;
-                public TextView t3;
-
-                public ViewHolder(View rootView) {
-                    super(rootView);
-                    this.rootView = rootView;
-                    this.t1 = (TextView) rootView.findViewById(R.id.t1);
-                    this.t2 = (TextView) rootView.findViewById(R.id.t2);
-                    this.t3 = (TextView) rootView.findViewById(R.id.t3);
-                }
-
-            }
-
-            @NonNull
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-                return new ViewHolder(LayoutInflater.from(getActivity()).inflate(R.layout.item_1, viewGroup, false));
+            public void run() {
+                sw.setRefreshing(false);
+                rec.setAdapter(new RecyclerView.Adapter() {
+                    class ViewHolder extends RecyclerView.ViewHolder {
+                        public View rootView;
+                        public TextView t1;
+                        public TextView t2;
+                        public TextView t3;
+
+                        public ViewHolder(View rootView) {
+                            super(rootView);
+                            this.rootView = rootView;
+                            this.t1 = (TextView) rootView.findViewById(R.id.t1);
+                            this.t2 = (TextView) rootView.findViewById(R.id.t2);
+                            this.t3 = (TextView) rootView.findViewById(R.id.t3);
+                        }
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                        return new ViewHolder(LayoutInflater.from(getActivity()).inflate(R.layout.item_1, viewGroup, false));
+                    }
+
+                    @Override
+                    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                        ViewHolder holder = (ViewHolder) viewHolder;
+                        holder.t1.setText(list.get(i).getTitle());
+                        holder.t2.setText(list.get(i).getContent());
+                        holder.t3.setText(list.get(i).getSub());
+                    }
+
+                    @Override
+                    public int getItemCount() {
+                        return list.size();
+                    }
+
+
+                });
             }
-
-            @Override
-            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-                ViewHolder holder = (ViewHolder) viewHolder;
-                holder.t1.setText(list.get(i).getTitle());
-                holder.t2.setText(list.get(i).getContent());
-                holder.t3.setText(list.get(i).getSub());
-            }
-
-            @Override
-            public int getItemCount() {
-                return list.size();
-            }
-
-
-        });
+        }, 1500);
         return view;
     }
 
